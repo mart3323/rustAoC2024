b2661966e5883b88551ea8e37fe7528329fae40d
Mart Jõgi <Mart3323@gmail.com>
2024-12-01 23:01:25 +0200

    day 1 (Historian Hysteria): Parse with nom

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,30 +1,42 @@
 use std::cmp::Reverse;
 use std::collections::BinaryHeap;
 use std::error::Error;
+use std::str::FromStr;
+use nom;
+use nom::{IResult, Parser};
+
+// region input
+// Using nom, parse out an integer value
+fn parse_line(input: &str) -> IResult<&str, (i32, i32)> {
+    (
+        nom::character::complete::digit1.map_res(i32::from_str),
+        nom::character::complete::multispace1,
+        nom::character::complete::digit1.map_res(i32::from_str),
+    )
+        .map(|(left, _, right)| { (left, right)})
+        .parse(input)
+}
+fn parse_file(input: &str) -> IResult<&str, Vec<(i32, i32)>> {
+    nom::multi::separated_list1(nom::character::complete::line_ending, parse_line)
+        .parse(input)
+}
+// endregion
 
 pub struct Day1 {
 
 }
 type Input = (BinaryHeap<Reverse<i32>>, BinaryHeap<Reverse<i32>>);
 
+
 impl Day1 {
     fn parse_file(&self, input: &String) -> Result<Input, Box<dyn Error>> {
         let mut left = BinaryHeap::new();
         let mut right = BinaryHeap::new();
-        let lines = input.split("\n");
-        for line in lines {
-            let parts = line.split_whitespace();
-            for (i, part) in parts.enumerate() {
-                if i == 0 {
-                    left.push(Reverse(part.parse()?));
-                } else if i == 1 {
-                    right.push(Reverse(part.parse()?));
-                } else {
-                    return Err("More than 2 values found on a single line of the input file")?
-                }
-            }
+        let result1 = parse_file(input);
+        for (l, r) in result1.unwrap().1 {
+            left.push(Reverse(l));
+            right.push(Reverse(r));
         }
-
         Ok((left, right))
     }
 }
