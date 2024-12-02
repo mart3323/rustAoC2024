37a4e81631bfc397a45038b2c41cd972dc2649a3
Mart Jõgi <Mart3323@gmail.com>
2024-12-02 21:52:51 +0200

    day 2 (Red-Nosed Reports): Part 2 - As submitted

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -1,9 +1,7 @@
+use crate::utils::read_input_files;
 use nom;
 use nom::{IResult, Parser};
-use std::error::Error;
-use std::fmt::Display;
 use std::str::FromStr;
-use crate::utils::read_input_files;
 
 // region input
 type Level = i32;
@@ -12,13 +10,12 @@ type Input = Vec<Report>;
 fn parse_line(input: &str) -> IResult<&str, Report> {
     nom::multi::separated_list1(
         nom::character::complete::space1,
-        nom::character::complete::digit1.map_res(i32::from_str)
+        nom::character::complete::digit1.map_res(i32::from_str),
     )
-        .parse(input)
+    .parse(input)
 }
 fn parse_file(input: &str) -> IResult<&str, Input> {
-    nom::multi::separated_list1(nom::character::complete::line_ending, parse_line)
-        .parse(input)
+    nom::multi::separated_list1(nom::character::complete::line_ending, parse_line).parse(input)
 }
 // endregion
 pub fn solve_day2() {
@@ -37,18 +34,8 @@ pub fn solve_day2() {
         let actual = solve2(&demo);
         let expected = inputs.expected2;
         assert_eq!(actual, expected);
-        let solution = solve(&full);
-        println!("Part1: {}", solution);
-    }
-}
-
-pub struct Day2 {
-
-}
-
-impl Day2 {
-    fn parse_file(&self, input: &str) -> Result<Input, Box<dyn Error>> {
-        Ok(parse_file(input).unwrap().1)
+        let solution = solve2(&full);
+        println!("Part2: {}", solution);
     }
 }
 
@@ -57,8 +44,7 @@ fn report_is_safe(report: &Report) -> bool {
         return true;
     }
 
-    let diffs = report.windows(2)
-        .map(|pair| { pair[1]-pair[0] });
+    let diffs = report.windows(2).map(|pair| pair[1] - pair[0]);
     let mut expect_signum = 0;
 
     for diff in diffs {
@@ -69,20 +55,115 @@ fn report_is_safe(report: &Report) -> bool {
             return false;
         }
         let abs = diff.abs();
-        if ! (1 <= abs && abs <= 3) {
+        if !(1 <= abs && abs <= 3) {
             return false;
         }
     }
     return true;
 }
+fn valid_pair(a: i32, b: i32) -> bool {
+    let diff = (b - a).abs();
+    1 <= diff && diff <= 3
+}
+
+fn report_is_safeish(report: &Report) -> bool {
+    #[derive(PartialEq, Debug)]
+    enum ErrorLocation {
+        Unknown,
+        Between(usize, usize),
+        Error,
+    }
+    impl ErrorLocation {
+        fn narrow(&self, start: usize, end: usize) -> ErrorLocation {
+            let newvalue = self._narrow(start, end);
+            return newvalue;
+        }
+        fn _narrow(&self, start: usize, end: usize) -> ErrorLocation {
+            match self {
+                ErrorLocation::Unknown => ErrorLocation::Between(start, end),
+                ErrorLocation::Between(from, to) => {
+                    let &from = from.max(&start);
+                    let &to = to.min(&(end));
+                    if to > from {
+                        ErrorLocation::Between(from, to)
+                    } else {
+                        ErrorLocation::Error
+                    }
+                }
+                _ => ErrorLocation::Error,
+            }
+        }
+    }
+
+    if report.is_empty() {
+        return true;
+    }
+    let mut error_location = ErrorLocation::Unknown;
+
+    for (i, pair) in report.windows(3).enumerate() {
+        let a = pair[0];
+        let b = pair[1];
+        let c = pair[2];
+        if (b - a).signum() != (c - b).signum() {
+            error_location = error_location.narrow(i, i + 2)
+        }
+        if !valid_pair(a, b) {
+            error_location = error_location.narrow(i, i + 1)
+        }
+        if !valid_pair(b, c) {
+            error_location = error_location.narrow(i + 1, i + 2)
+        }
+        if error_location == ErrorLocation::Error {
+            return false;
+        }
+    }
+    match error_location {
+        ErrorLocation::Error => false,
+        ErrorLocation::Between(start, end) => {
+            for i in start..end+1 {
+                if i == 0 {
+                    return true;
+                } else if i == report.len()-1 {
+                    return true;
+                } else {
+                    if let Some(left) = report.get(i - 1) {
+                        if let Some(right) = report.get(i + 1) {
+                            if valid_pair(*left, *right) {
+                                let mut subreport = report.clone();
+                                subreport.remove(i);
+                                return report_is_safe(&subreport);
+                            }
+                        }
+                    }
+                }
+            }
+            false
+        }
+        ErrorLocation::Unknown => true,
+    }
+}
+fn report_is_safeish_dumb_version(report: &Report) -> bool {
+    for i in 0..report.len() {
+        let mut candidate = report.clone();
+        candidate.remove(i);
+        if report_is_safe(&candidate) {
+            return true;
+        }
+    }
+    return false;
+}
 
 fn solve(reports: &Input) -> String {
     reports
         .iter()
-        .filter(|r| {report_is_safe(r)})
+        .filter(|r| report_is_safe(r))
         .count()
         .to_string()
 }
 fn solve2(reports: &Input) -> String {
-    todo!()
+    reports
+        .iter()
+        .filter(|r| report_is_safeish_dumb_version(r))
+        .count()
+        .to_string()
 }
