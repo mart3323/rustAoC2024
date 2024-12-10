56b202d020884773c5b16b25717429d64cb2b5bb
Mart Jõgi <Mart3323@gmail.com>
2024-12-10 21:42:21 +0200

    day 10 (Hoof it): Both parts - As submitted

---
+++ src/day10/mod.rs
@@ -0,0 +1,103 @@
+use std::collections::HashSet;
+use crate::utils::read_input_file;
+
+const DAY: &str = "day10";
+
+#[derive(Debug, Eq, PartialEq, Hash, Copy, Clone)]
+struct Pos {
+    x: usize,
+    y: usize
+}
+impl Pos {
+    fn get_neighbors(&self) -> Vec<Pos> {
+        let mut neighbors = Vec::with_capacity(4);
+        neighbors.push(Pos{x: self.x, y: self.y + 1});
+        neighbors.push(Pos{x: self.x + 1, y: self.y});
+        if self.y > 0 {
+            neighbors.push(Pos{x: self.x, y: self.y - 1});
+        }
+        if self.x > 0 {
+            neighbors.push(Pos{x: self.x - 1, y: self.y});
+        }
+        return neighbors;
+    }
+    fn read_from(&self, map: &Vec<Vec<char>>) -> Option<char> {
+        let row = map.get(self.y)?;
+        let c = row.get(self.x)?;
+        return Some(*c);
+    }
+}
+impl From<(usize, usize)> for Pos {
+    fn from((x, y): (usize, usize)) -> Pos {
+        Pos{x, y}
+    }
+}
+
+fn score_trailheads(map: &Vec<Vec<char>>) -> usize {
+    let width = map.get(0).unwrap().len();
+    let height = map.len();
+    let positions = (0..width).flat_map(|x|
+        (0..height).map(move |y| Pos::from((x,y)))
+    );
+    let zero_positions = positions.filter(|v| v.read_from(&map).is_some_and(|v| v == '0'));
+
+    let score = zero_positions.map(|pos| -> usize {
+        let mut neighbors = HashSet::from([pos]);
+        for i in vec!['1', '2', '3', '4', '5', '6', '7', '8', '9'] {
+            neighbors = neighbors.iter()
+                .flat_map(|pos| pos.get_neighbors())
+                .filter(|pos| pos.read_from(&map).is_some_and(|c| c == i))
+                .collect();
+        };
+        return neighbors.len();
+    }).sum();
+
+    return score;
+}
+fn rate_trailheads(map: &Vec<Vec<char>>) -> usize {
+    let width = map.get(0).unwrap().len();
+    let height = map.len();
+    let positions = (0..width).flat_map(|x|
+        (0..height).map(move |y| Pos::from((x,y)))
+    );
+    let zero_positions = positions.filter(|v| v.read_from(&map).is_some_and(|v| v == '0'));
+
+    let score = zero_positions.map(|pos| -> usize {
+        let mut neighbors = vec!(pos);
+        for i in vec!['1', '2', '3', '4', '5', '6', '7', '8', '9'] {
+            neighbors = neighbors.iter()
+                .flat_map(|pos| pos.get_neighbors())
+                .filter(|pos| pos.read_from(&map).is_some_and(|c| c == i))
+                .collect();
+        };
+        return neighbors.len();
+    }).sum();
+
+    return score;
+}
+#[test]
+fn demo1() {
+    let demo = read_input_file(DAY, "demo.txt");
+    let lines: Vec<Vec<char>> = demo.split_whitespace().map(|line| line.chars().collect()).collect();
+
+    let score = score_trailheads(&lines);
+    assert_eq!(score, 36usize)
+}
+
+#[test]
+fn demo2() {
+    let demo = read_input_file(DAY, "demo.txt");
+    let lines: Vec<Vec<char>> = demo.split_whitespace().map(|line| line.chars().collect()).collect();
+
+    let score = rate_trailheads(&lines);
+    assert_eq!(score, 81usize)
+}
+
+pub fn solve_day10() {
+    let full = read_input_file(DAY, "full.txt");
+    let lines: Vec<Vec<char>> = full.split_whitespace().map(|line| line.chars().collect()).collect();
+    let score = score_trailheads(&lines);
+    println!("Part1: {}", score);
+    let rating = rate_trailheads(&lines);
+    println!("Part2: {}", rating);
+}
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ mod day7;
 mod day8;
 mod utils;
 mod day9;
+mod day10;
 
 use std::ops::Deref;
 use std::sync::Arc;
@@ -17,6 +18,7 @@ use std::sync::mpsc::channel;
 use std::thread;
 use std::time::Duration;
 use crate::day0::solve_day0;
+use crate::day10::solve_day10;
 use crate::day2::solve_day2;
 use crate::day3::solve_day3;
 use crate::day4::solve_day4;
@@ -58,12 +60,14 @@ fn main() {
     // solve_day6();
     // solve_day7();
     // solve_day8();
-    solve_day9();
+    // solve_day9();
+    solve_day10();
 }
 
 fn bench() {
-    let time9 = benchmark(solve_day9, "day7");
-    let time8 = benchmark(solve_day8, "day7");
+    let time10 = benchmark(solve_day10, "day10");
+    let time9 = benchmark(solve_day9, "day9");
+    let time8 = benchmark(solve_day8, "day8");
     let time7 = benchmark(solve_day7, "day7");
     let time6 = benchmark(solve_day6, "day6");
     let time5 = benchmark(solve_day5, "day5");
@@ -72,6 +76,7 @@ fn bench() {
     let time2 = benchmark(solve_day2, "day2");
     let time0 = benchmark(solve_day0, "day0");
 
+    println!("time9: {}", time10);
     println!("time9: {}", time9);
     println!("time8: {}", time8);
     println!("time7: {}", time7);
