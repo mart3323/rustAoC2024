48d525f91fa96abd19241e067473706485314706
Mart Jõgi <Mart3323@gmail.com>
2025-01-04 18:32:50 +0200

    Day 21 (Keypad Conundrum): Part 2 - As submitted

--- src/keypadConundrum/mod.rs
+++ src/keypadConundrum/mod.rs
@@ -11,12 +11,13 @@ fn shortest(a: String, b: String) -> String {
 }
 
 struct DirCache(HashMap<(DirpadKey, DirpadKey), String>);
+struct DirCacheOptimized(HashMap<(DirpadKey, DirpadKey), usize>);
 
 impl Default for DirCache {
     fn default() -> Self {
         use DirpadKey::*;
         let options = [A, Up, Down, Left, Right];
-        let mut out = DirCache(HashMap::new());
+        let mut out = Self(HashMap::new());
         for start in options {
             for end in options {
                 out.0.insert((start, end), match end {
@@ -31,8 +32,25 @@ impl Default for DirCache {
         return out;
     }
 }
-// Ours: <vA<AA>>^AvAA^<A>A v<<A>>^AvA^A <vA^>A<A>A v<<A>A^>A<Av>A^A
-// Ref:  <vA<AA>>^AvAA<^A>A <v<A>>^AvA^A <vA>^A<v<A >^A>AAvA^A<v<A>A>^AAAvA<^A>A
+impl Default for DirCacheOptimized {
+    fn default() -> Self {
+        use DirpadKey::*;
+        let options = [A, Up, Down, Left, Right];
+        let mut out = Self(HashMap::new());
+        for start in options {
+            for end in options {
+                out.0.insert((start, end), match end {
+                    A => 1,
+                    Up => 1,
+                    Right => 1,
+                    Down => 1,
+                    Left => 1,
+                });
+            }
+        };
+        return out;
+    }
+}
 impl DirCache {
     // Path to go from X to Y
     fn leg(&self, from: DirpadKey, to: DirpadKey) -> String {
@@ -159,6 +177,131 @@ impl DirCache {
         return out;
     }
 }
+impl DirCacheOptimized {
+    // Path to go from X to Y
+    fn leg(&self, from: DirpadKey, to: DirpadKey) -> usize {
+        return self.0.get(&(from, to)).unwrap().to_owned();
+    }
+    // Path to go from A to _ times press _ time and return
+    fn sequence1(&self, to: DirpadKey, press_count: usize) -> usize {
+        self.leg(DirpadKey::A, to) +
+            &self.leg(to, to) * (press_count - 1) +
+            &self.leg(to, DirpadKey::A)
+    }
+    // Path to go from A to _ times press _ and _ times press _ and return
+    fn sequence2(&self, to_1: DirpadKey, press_count_1: usize, to_2: DirpadKey, press_count_2: usize) -> usize {
+        self.leg(DirpadKey::A, to_1)
+            + (&self.leg(to_1, to_1) * (press_count_1 - 1))
+            + &self.leg(to_1, to_2)
+            + (&self.leg(to_2, to_2) * (press_count_2 - 1))
+            + &self.leg(to_2, DirpadKey::A)
+    }
+    fn sequence2_unordered(&self, to_1: DirpadKey, press_count_1: usize, to_2: DirpadKey, press_count_2: usize) -> usize {
+        self.sequence2(to_1, press_count_1, to_2, press_count_2).min(
+            self.sequence2(to_2,press_count_2, to_1, press_count_1)
+        )
+    }
+    fn next(&self) -> Self {
+        use DirpadKey::*;
+        let options = [A,Up,Down,Left,Right];
+        let mut out = DirCacheOptimized(HashMap::new());
+        for start in options {
+            for end in options {
+                out.0.insert((start,end), match (start, end) {
+                    // Left
+                    (Down, Left)|
+                    (Right,Down)|
+                    (A,Up) => self.sequence1(Left, 1),
+                    // Left Left
+                    (Right, Left) => self.sequence1(Left, 2),
+                    // Down Left (ordered)
+                    (Up,Left) => self.sequence2(Down, 1, Left, 1),
+                    // Down Left (unordered)
+                    (A, Down) => self.sequence2_unordered(Down, 1, Left, 1),
+                    // Down Left Left
+                    (A, Left) => self.sequence2(Down, 1, Left, 2),
+                    // Up
+                    (Down, Up)|(Right,A) => self.sequence1(Up, 1),
+                    // Right
+                    (Left,Down)|
+                    (Down,Right)|
+                    (Up,A) => self.sequence1(Right, 1),
+                    // Right Right
+                    (Left, Right) => self.sequence1(Right, 2),
+                    // Right Up (ordered)
+                    (Left, Up) => self.sequence2(Right, 1, Up, 1),
+                    // Right Up (unordered)
+                    (Down, A) => self.sequence2_unordered(Right, 1, Up, 1),
+                    // Right Right Up
+                    (Left, A) => self.sequence2(Right, 2, Up, 1),
+                    // Down
+                    (Up,Down)|
+                    (A,Right) => self.sequence1(Down, 1),
+                    // Right down
+                    (Up, Right) => self.sequence2_unordered(Right, 1, Down, 1),
+                    // Left up
+                    (Right, Up) => self.sequence2_unordered(Left, 1, Up, 1),
+                    (a, b) if a == b => 1,
+                    (x,y) => panic!("Match was not exhaustive, failed to cover {:?} to {:?}", &x, &y)
+                });
+
+            }
+        };
+        return out;
+    }
+
+    fn path_for_code(&self, code: &Vec<NumpadKey>) -> usize {
+        use DirpadKey::*;
+
+        let mut out = 0;
+        let mut pos = NumpadKey::A;
+        for &key in code {
+            fn pos_of(k: &NumpadKey) -> (i8, i8) {
+                use NumpadKey::*;
+                match k {
+                    Seven => (0,0), Eight => (1,0), Nine  => (2,0),
+                    Four  => (0,1), Five  => (1,1), Six   => (2,1),
+                    One   => (0,2), Two   => (1,2), Three => (2,2),
+                                     Zero => (1,3),     A => (2,3),
+                }
+            }
+            let start = pos_of(&pos);
+            let end = pos_of(&key);
+            let dx = end.0 - start.0;
+            let dy = end.1 - start.1;
+            // Exception, do not cut the corner
+            if pos == NumpadKey::Zero && dx < 0 || pos == NumpadKey::A && dx == -2 {
+                assert!(dy < 0);
+                out += &self.sequence2(Up, -dy as usize, Left, -dx as usize)
+            } else if key == NumpadKey::Zero && dx > 0 || key == NumpadKey::A && dx == 2 {
+                assert!(dy > 0);
+                out += &self.sequence2(Right, dx as usize, Down, dy as usize)
+            } else if dx < 0 && dy < 0 {
+                out += &self.sequence2_unordered(Up, -dy as usize, Left, -dx as usize)
+            } else if 0 < dx && dy < 0 {
+                out += &self.sequence2_unordered(Up, -dy as usize, Right, dx as usize)
+            } else if 0 < dx && 0 < dy {
+                out += &self.sequence2_unordered(Down, dy as usize, Right, dx as usize)
+            } else if dx < 0 && 0 < dy {
+                out += &self.sequence2_unordered(Down, dy as usize, Left, -dx as usize)
+            } else if dx == 0 {
+                if dy > 0 {
+                    out += &self.sequence1(Down, dy as usize);
+                } else {
+                    out += &self.sequence1(Up, -dy as usize);
+                }
+            } else if dy == 0 {
+                if dx > 0 {
+                    out += &self.sequence1(Right, dx as usize);
+                } else {
+                    out += &self.sequence1(Left, -dx as usize);
+                }
+            }
+            pos = key;
+        }
+        return out;
+    }
+}
 
 // Ours: <vA<AA>>^AvAA^<A>A v<<A>>^AvA^A v<<A>>^AA<vA>A^A<A>A v<<A>A^>AAA<Av>A^A
 // Ref:  <vA<AA>>^AvAA<^A>A <v<A>>^AvA^A <vA>^A<v<A>^A>AAvA^A <v<A>A>^AAAvA<^A>A
@@ -220,6 +363,18 @@ fn test_part_1() {
     println!("full compl: {}", result);
     assert!(156544 < result);
 }
+#[test]
+fn test_solvers_are_equivalent() {
+    let solver3 = DirCache::default().next().next();
+    let osolver3 = DirCacheOptimized::default().next().next();
+    for i in DEMO_INPUT {
+        let code = parse_code(i).expect("Code to be valid");
+        assert_eq!(
+            solver3.path_for_code(&code).chars().count(),
+            osolver3.path_for_code(&code)
+        )
+    }
+}
 pub fn part1() -> usize {
     let mut total_complexity = 0;
     let solver_3 = DirCache::default().next().next();
@@ -233,7 +388,7 @@ pub fn part1() -> usize {
 }
 pub fn part2() -> usize {
     let mut total_complexity = 0;
-    let mut solver = DirCache::default();
+    let mut solver = DirCacheOptimized::default();
     for i in 0..25 {
         solver = solver.next();
     }
@@ -241,7 +396,7 @@ pub fn part2() -> usize {
     for code in FULL_INPUT {
         let value: usize = code.trim_start_matches("0").trim_end_matches("A").parse().expect("Code to be valid");
         let code = parse_code(code).expect("Code to be valid");
-        let length: usize = solver_26.path_for_code(&code).chars().count();
+        let length: usize = solver_26.path_for_code(&code);
         total_complexity += value * length;
     }
     return total_complexity
