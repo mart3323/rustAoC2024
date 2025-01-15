7a2f9a852ac405995fb5a7b1f8b0c7914a4f9ff7
Mart Jõgi <Mart3323@gmail.com>
2025-01-15 14:32:15 +0200

    refactor day3

--- src/day03 - Mull It Over/mod.rs
+++ src/day03 - Mull It Over/mod.rs
@@ -1,38 +1,126 @@
-mod parse;
 mod test;
-
-use utils::read_input_file;
 
 const FULL: &'static str = include_str!("inputs/full.txt");
 
-fn parse_full_part1() -> Vec<parse::Mul> {
-    parse::parse_part1(FULL).expect("Full input to parse")
-}
-#[test]
-fn full_input_parses_part1() {
-    parse_full_part1();
-}
 
-fn parse_full_part2() -> Vec<parse::Mul> {
-    parse::parse_part1(FULL).expect("Full input to parse")
+pub fn solve_part1(input: &str) -> usize {
+    let mut remainder = input;
+    let mut total = 0;
+    while let Some(index) = remainder.chars().position(|c| c == 'm') {
+        let a : usize;
+        let b : usize;
+        // consume until m
+        remainder = &remainder[index..];
+        // consume mul(
+        if !remainder.starts_with("mul(") {
+            remainder = &remainder[1..];
+            continue;
+        }
+        remainder = &remainder[4..];
+        // consume first number
+        match remainder.find(|c: char| !c.is_numeric()) {
+            None => {
+                remainder = "";
+                continue;
+            },
+            Some(index) => {
+                a = remainder[0..index].parse().unwrap();
+                remainder = &remainder[index..];
+            }
+        }
+        // consume comma
+        match remainder.chars().next() {
+            Some(',') => {remainder = &remainder[1..]}
+            _ => { continue; }
+        }
+        // consume second number
+        match remainder.find(|c: char| !c.is_numeric()) {
+            None => {
+                remainder = "";
+                continue;
+            },
+            Some(index) => {
+                b = remainder[0..index].parse().unwrap();
+                remainder = &remainder[index..];
+            }
+        }
+        // consume close paren
+        match remainder.chars().next() {
+            Some(')') => { remainder = &remainder[1..]}
+            _ => { continue; }
+        }
+        // multiply and update total
+        total += a * b
+    }
+    return total;
 }
-#[test]
-fn full_input_parses_part2() {
-    parse_full_part2();
+pub fn solve_part2(input: &str) -> usize {
+
+    let mut remainder = input;
+    let mut total = 0;
+    let mut off = false;
+    while let Some(index) = remainder.chars().position(|c| (!off && c == 'm') || c == 'd') {
+        let a : usize;
+        let b : usize;
+        // Parse comment tokens
+        remainder = &remainder[index..];
+        if remainder.starts_with("don't()") {
+            off = true;
+            remainder = &remainder["don't()".len()..];
+            continue;
+        } else if remainder.starts_with("do()") {
+            off = false;
+            remainder = &remainder["do()".len()..];
+            continue;
+        }
+        
+        // consume mul(
+        if !remainder.starts_with("mul(") {
+            remainder = &remainder[1..];
+            continue;
+        }
+        remainder = &remainder[4..];
+        // consume first number
+        match remainder.find(|c: char| !c.is_numeric()) {
+            None => {
+                remainder = "";
+                continue;
+            },
+            Some(index) => {
+                a = remainder[0..index].parse().unwrap();
+                remainder = &remainder[index..];
+            }
+        }
+        // consume comma
+        match remainder.chars().next() {
+            Some(',') => {remainder = &remainder[1..]}
+            _ => { continue; }
+        }
+        // consume second number
+        match remainder.find(|c: char| !c.is_numeric()) {
+            None => {
+                remainder = "";
+                continue;
+            },
+            Some(index) => {
+                b = remainder[0..index].parse().unwrap();
+                remainder = &remainder[index..];
+            }
+        }
+        // consume close paren
+        match remainder.chars().next() {
+            Some(')') => { remainder = &remainder[1..]}
+            _ => { continue; }
+        }
+        // multiply and update total
+        total += a * b
+    }
+    return total;
 }
-
-
-fn solve(input: Vec<parse::Mul>) -> usize {
-    input
-        .into_iter()
-        .map(|mul| mul.value())
-        .sum()
-}
-
 
 pub fn part1() -> usize {
-    solve(parse_full_part1())
+    solve_part1(FULL)
 }
 pub fn part2() -> usize {
-    solve(parse_full_part2())
+    solve_part2(FULL)
 }
--- src/day03 - Mull It Over/test.rs
+++ src/day03 - Mull It Over/test.rs
@@ -1,29 +1,17 @@
 #[cfg(test)]
 mod test {
-    use crate::{parse, solve};
-    use crate::parse::Mul;
-
+    use crate::{solve_part1, solve_part2};
+    
     const DEMO: &'static str = include_str!("inputs/demo.txt");
     const DEMO2: &'static str = include_str!("inputs/demo2.txt");
 
-    fn parse_demo() -> Vec<Mul> {
-        parse::parse_part1(DEMO).expect("Demo input should parse")
-    }
-    fn parse_demo2() -> Vec<Mul> {
-        parse::parse_part1(DEMO2).expect("Demo input should parse")
-    }
-    #[test]
-    fn demo_inputs_parse() {
-        parse_demo();
-        parse_demo2();
-    }
     #[test]
     fn test_part1() {
-        assert_eq!(solve(parse_demo()), 161);
+        assert_eq!(solve_part1(DEMO), 161);
     }
     #[test]
     fn test_solve_advanced() {
-        assert_eq!(solve(parse_demo2()), 48);
+        assert_eq!(solve_part2(DEMO2), 48);
     }
 }
 #[cfg(test)]
